db19232afbcf997d971f621480ef28c055804ff3
Cheng Kwing Ngai <ckwingngai@gmail.com>
2017-05-23 12:33:32 +0800

    add react route

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 import App from './App';
+import Counter from './counter/counter';
+import Todolist from './todolist/todolist';
 import rootReducer from './rootReducer'
 import './index.css';
 
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/counter" component={Counter} />
+        <Route path="/todolist" component={Todolist} />
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
